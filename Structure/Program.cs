e16e89ce2e4c779cf0a9ff8c2e3ff6fa5c8c11cd
Anni-Ritter <UmeliaRock18>
2020-11-04 18:02:00 +0300

    init project

---
+++ Structure/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new Stack<string>();
+            people.Push("Tom");
+            people.Push("Bob");
+            people.Push("Sam");
+            people.Push("Mark");
+            people.Push("Felix");
+            people.Push("I.N.");
+            people.Push("Bang Chan");
+            people.Push("Han");
+            people.Push("Minho");
+            people.Push("Alex");
+            people.Push("Ten");
+
+
+            foreach (var items in people)
+            {  
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
---
+++ Structure/Stack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Structure
+{
+    class Stack<T>: IEnumerable<T>
+    {
+        private T[] items;
+        private int count;
+        const int number = 10;
+        public Stack()
+        {
+            items = new T[number];
+        }
+
+
+        public bool IsEmpty
+        {
+            get { return count == 0; }
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void Push(T item)
+        {
+            if (count == items.Length)
+                Resize(items.Length + 10);
+
+            items[count++] = item;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+            T item = items[--count];
+            items[count] = default(T); 
+
+            if (count > 0 && count < items.Length - 10)
+                Resize(items.Length - 10);
+
+            return item;
+        }
+
+        public T Peek()
+        {
+            return items[count - 1];
+        }
+
+        private void Resize(int max)
+        {
+            T[] tempItems = new T[max];
+            for (int i = 0; i < count; i++)
+                tempItems[i] = items[i];
+            items = tempItems;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                yield return this.items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
